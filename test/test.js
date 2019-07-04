81c5117f73e065e19a6430d1e8f059a391984460
Jaid <jaid.jsx@gmail.com>
2019-07-04 05:11:05 +0200

    Extended test
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -11,14 +11,19 @@ const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.jo
 const {default: PollingEmitter} = indexModule
 
 it("should run", async () => {
+  let newEntryFired = false
   const emitter = new PollingEmitter({
     pollIntervalSeconds: 5,
     fetchEntries: () => [{key: "a"}],
     getIdFromEntry: ({key}) => key,
+  })
+  emitter.on("newEntry", () => {
+    newEntryFired = true
   })
   expect(emitter.processedEntryIds.size).toBe(0)
   await delay(ms`6 seconds`)
   expect(emitter.processedEntryIds.size).toBe(1)
   emitter.stop()
   expect(emitter.interval).toBe(undefined)
+  expect(newEntryFired).toBeTruthy()
 }, ms`10 seconds`)
